001de1f075d638cac8769c0cf3e0e222f6bb1798
Mohamedmorgan26 <mohamedmorgan0100@gmail.com>
2022-08-15 20:02:47 +0200

    Admin interface

---
+++ shop/shop/orders.js
@@ -0,0 +1,48 @@
+﻿const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        shipping: 'Peading'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        PaymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Drone',
+        productNumber: '49347',
+        PaymentStatus: 'Due',
+        shipping: 'Peading'
+    },
+    {
+        productName: 'Drone with camera Drone',
+        productNumber: '96996',
+        PaymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        PaymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Dji AIr 25',
+        productNumber: '81475',
+        PaymentStatus: 'Due',
+        shipping: 'Peading'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        PaymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+
+
+
+
+]
